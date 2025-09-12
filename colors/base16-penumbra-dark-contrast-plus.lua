0267cb7938ed906d3f1877bd04456caf2e989650
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-09-12 09:06:45 +0200

    feat: update themes

---
+++ colors/base16-penumbra-dark-contrast-plus.lua
@@ -0,0 +1,20 @@
+require("base16").setup({
+	base00 = "#181B1F",
+	base01 = "#24272B",
+	base02 = "#3E4044",
+	base03 = "#636363",
+	base04 = "#9E9E9E",
+	base05 = "#CECECE",
+	base06 = "#FFF7ED",
+	base07 = "#FFFDFB",
+	base08 = "#DF7F78",
+	base09 = "#CE9042",
+	base0A = "#9CA748",
+	base0B = "#50B584",
+	base0C = "#00B3C2",
+	base0D = "#61A3E6",
+	base0E = "#A48FE1",
+	base0F = "#D080B6",
+})
+
+vim.g.colors_name = "base16-penumbra-dark-contrast-plus"
